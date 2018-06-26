24bffc84d22c0f4c757c71e2e9fc3cfc098dde72
Trip Richert <trip.richert@gmail.com>
2018-06-26 08:10:05 -0500

    removed entity encapsulating axistimulus process.

--- tests/adder/elaborate.sh
+++ tests/adder/elaborate.sh
@@ -4,6 +4,5 @@
 /home/bin/bin/ghdl -a --std=08 ../../hdl/from_str_pkg.vhd
 /home/bin/bin/ghdl -a --std=08 ../../hdl/str_lst_pkg.vhd
 /home/bin/bin/ghdl -a --std=08 ../../hdl/stimulus_pkg.vhd
-/home/bin/bin/ghdl -a --std=08 ../../hdl/axistream_stim_unsigned.vhd
 /home/bin/bin/ghdl -a --std=08 tb/test_unsigned_adder.vhd
 /home/bin/bin/ghdl -e --std=08 test_unsigned_adder
--- tests/adder/tb/test_unsigned_adder.vhd
+++ tests/adder/tb/test_unsigned_adder.vhd
@@ -1,9 +1,31 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use work.str_lst_pkg.all;
+use work.from_str_pkg.all;
 
 entity test_unsigned_adder is
   generic ( filename : string := "testname.txt");
+
+  constant elemPerLine : natural := 2;
+  constant bitsPerElem : natural := 4;
+
+  type elemType is array(elemPerLine - 1 downto 0) of unsigned(bitsPerElem - 1 downto 0);
+
+  function fromStr(val : string) return elemType is
+    variable retVal : elemType;
+  begin
+    assert(elemPerLine = getLstNumSeg(val, string'(" ")))
+      report "wrong number of elements per line" severity warning;
+    for i in 0 to elemPerLine - 1 loop
+      retVal(i) := strToUnsigned(getLstSeg(val, i, string'(" ")), bitsPerElem);
+    end loop;
+    return retVal;
+  end function fromStr;
+  
+  package stimulus_pkg_Unsigned is new work.stimulus_pkg generic map(elemType, fromStr);
+  use stimulus_pkg_Unsigned.axiStreamStimulus;
+
 end entity test_unsigned_adder;
 
 architecture behavioral of test_unsigned_adder is
@@ -13,33 +35,38 @@ architecture behavioral of test_unsigned_adder is
   signal tlast  : std_ulogic;
   signal done   : std_ulogic := '0';
 
-  component axistream_stim_unsigned is
-  generic (
-    filename : string;
-    elemPerLine : natural := 2;
-    bitsPerElem : natural := 4
-    );
-  port (
-    clk : in std_ulogic;
-    tvalid : out std_ulogic;
-    tready : in std_ulogic;
-    tlast  : out std_ulogic;
-    done   : out std_ulogic
-    );
-  end component axistream_stim_unsigned;
-
   constant clk_period : time := 10 ns;
+  signal tdata : elemType;
+
 begin
 
-  assu_0 : axistream_stim_unsigned
-    generic map (filename => filename)
-    port map (
+
+  process
+  begin
+    done <= '0';
+
+    axiStreamStimulus(
+      filename => filename,
       clk => clk,
+      edge => '1',
       tvalid => tvalid,
       tready => tready,
-      tlast  => tlast,
+      tdata => tdata,
+      tlast => tlast,
       done => done
       );
+  end process;
+
+  process(clk)
+  begin
+    if rising_Edge(clk) then
+      if tvalid = '1' and tready = '1' then
+        report string'("0: ") & integer'image(to_integer(tdata(0)));
+        report string'("1: ") & integer'image(to_integer(tdata(1)));
+      end if;
+    end if;
+  end process;
+
   
   process
   begin
