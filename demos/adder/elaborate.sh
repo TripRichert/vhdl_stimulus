924de060350cfb7f2ba3d2184928798744f84bb2
Trip Richert <trip.richert@gmail.com>
2018-07-04 08:51:31 -0500

    added ghdl to path. Removed machine specific paths from elaborate.sh script

--- demos/adder/elaborate.sh
+++ demos/adder/elaborate.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-/home/bin/bin/ghdl -a --std=08 ../../hdl/textio_ext.vhd
-/home/bin/bin/ghdl -a --std=08 ../../hdl/from_str_pkg.vhd
-/home/bin/bin/ghdl -a --std=08 ../../hdl/to_str_pkg.vhd
-/home/bin/bin/ghdl -a --std=08 ../../hdl/str_lst_pkg.vhd
-/home/bin/bin/ghdl -a --std=08 ../../hdl/stimulus_pkg.vhd
-/home/bin/bin/ghdl -a --std=08 ../../hdl/log_pkg.vhd
+ghdl -a --std=08 ../../hdl/textio_ext.vhd
+ghdl -a --std=08 ../../hdl/from_str_pkg.vhd
+ghdl -a --std=08 ../../hdl/to_str_pkg.vhd
+ghdl -a --std=08 ../../hdl/str_lst_pkg.vhd
+ghdl -a --std=08 ../../hdl/stimulus_pkg.vhd
+ghdl -a --std=08 ../../hdl/log_pkg.vhd
 
-/home/bin/bin/ghdl -a --std=08 hdl/axistream_unsigned_adder.vhd
+ghdl -a --std=08 hdl/axistream_unsigned_adder.vhd
 
-/home/bin/bin/ghdl -a --std=08 tb/test_unsigned_adder.vhd
-/home/bin/bin/ghdl -e --std=08 test_unsigned_adder
+ghdl -a --std=08 tb/test_unsigned_adder.vhd
+ghdl -e --std=08 test_unsigned_adder
